39ef22e35d2a2b8071f5605608399a079624afca
Surya Suresh <ssuresh3@ucsc.edu>
2019-10-19 23:10:34 -0700

    updated Form.js

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -16,5 +16,38 @@ export default class Form extends Component{
     }
 
     //saving user info details
-    saveData   
+    saveData =async()=>{
+        const {email, password} = this.state;
+        
+        //saving data with asyncstorage
+        let loginDetails={
+            email: email,
+            password: password
+        }
+
+        //registers a new user to the app
+        if(this.props.type == Signup){ //may need to change back to != Login
+            AsyncStorage.setItem(loginDetails, JSON.stringify(loginDetails));
+            Keyboard.dismiss();
+            alert("You have successfully registered!");
+            this.login(); //where does this function come from?
+        }
+
+        //logs in an old user
+        else if(this.props.type == Login){
+            try{
+                let loginDetails = await AsyncStorage.getItem('loginDetails');
+                let ld = JSON.parse(loginDetails);
+                if(ld.email != null && ld.password != null){
+                    if(ld.email == email && ld.password == password){
+                        alert("Welcome!");
+                    } else{
+                        alert("Invalid email or password!");
+                    }
+                }
+            } catch{
+                alert(Error);
+            }
+        }
+    }
 }
