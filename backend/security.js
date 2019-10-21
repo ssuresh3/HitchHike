018be6f6dc0c2cbcc027d611d15f06adee3931db
Cody Hartsook <chartsoo@ucsc.edu>
2019-10-20 17:25:03 -0700

    adding security module

---
+++ backend/security.js
@@ -0,0 +1,39 @@
+// security module
+
+const crypto = require('crypto');
+const algorithm = 'sha256';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function encrypt(password) {
+	let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+	let encrypted = cipher.update(password);
+	encrypted = Buffer.concat([encrypted, cipher.final()]);
+	return encrypted.toString('hex');
+}
+
+function decrypt(password) {
+    let iv = Buffer.from(password.iv, 'hex');
+    let encryptedpassword = Buffer.from(password.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedpassword);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+module.exports = {
+
+    encryptPasword: function(password){
+        return encrypt(password)
+    }
+
+    decryptPassword: function(password){
+        return decrypt(password)
+    }
+
+    varifyUser: function(email){
+        console.log("varifying user given email: ", email)
+    }
+
+}
+
