cd8866b8824a0f3c1b6f6294d56ab78f9dba19ec
Surya Suresh <ssuresh3@ucsc.edu>
2019-10-20 20:56:09 -0700

    Form.js finished need to complete rest of the files

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import Form from './src/components/Form.js';
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return(<Form>
+      </Form>)
+  }
+}
