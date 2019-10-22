86b3e8e4b8b682018c61c553d765a05d44d5984d
Surya Suresh <ssuresh3@ucsc.edu>
2019-10-21 17:37:26 -0700

    Signup.js done

---
+++ frontend/src/Route.js
@@ -0,0 +1,6 @@
+//importing necessary libraries
+import React, {component} from 'react';
+import {Router, Stack, Scene} from 'react-native-router-flux';
+
+import Login from './src/pages/Login.js';
+import Signup from './src/pages/Signup.js';
