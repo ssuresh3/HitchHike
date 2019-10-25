a0c6013e8de52409b6c430ca27345bbd58944d78
Harshitha <harshitha.arulm@gmail.com>
2019-10-25 16:18:37 -0700

    Beautify login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,122 +11,51 @@ export default class Login extends Component {
     signup() {
         Actions.signup()
     }
-          constructor(props) {
+    constructor(props) {
         super(props);
-        this.state = {user: ''};
-        this.state = {pass: ''};
-      }
+        this.state = { user: '' };
+        this.state = { pass: '' };
+    }
     render() {
 
-        return(
-        <React.Fragment>
-      {/* <Image source = {pic} style = {{width: 193, height:110}}/> */}
-      {/* <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Text style={{color: 'orange'}}>Username</Text>
-
-        <TextInput
-          style={{ height: 40, borderColor: 'orange', borderWidth: 1 }}
-          placeholder="Type username!"
-          onChangeText={(user) => this.setState({user})}
-          value={this.state.user}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-       </Text>
-      </View>
-
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Text style={{color: 'orange'}}>Password</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'orange', borderWidth: 1 }}
-          secureTextEntry={true}
-          placeholder="Type password!"
-          onChangeText={(pass) => this.setState({pass})}
-          value={this.state.pass}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-       </Text>
-      </View> */}
-      <View style={styles.container}>
-                <TextInput style={styles.inputBox} //creating email text input
-                    onChangeText={(email) => this.setState({ email })}
-                    underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Username"
-                    placeholderTextColor="#ff8700"
-                    selectionColor="#fff"
-                    keyboardType="Username"
-                    onSubmitEditing={() => this.password.focus()} />
-                <TextInput style={styles.inputBox} //creating password text input
-                    onChangeText={(password) => this.setState({ password })}
-                    underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Password"
-                    secureTextEntry={true}
-                    placeholderTextColor="#ff8700"
-                    ref={(input) => this.password = input}
-                />
-                <TouchableOpacity style={styles.button}>
-                    {
-                        //depending on what the user presses, the button will either render login or signup
-                    }
-                    <Text style={styles.buttonText} onPress={this.saveData} color = "orange"> Login {this.props.type} </Text>
-                </TouchableOpacity>
-            </View>   
-      <View style={{alignItems: 'center', flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-        {/* <Button
-          title="Login!"
-          color="#ff8700"
-          //onPress={() => Alert.alert('Simple Button pressed')}
-        /> */}
-        <TouchableOpacity style={styles.button1}>
-                    {
-                        //depending on what the user presses, the button will either render login or signup
-                    }
-                    <Text style={styles.buttonText1} onPress={this.saveData}> Sign Up!{this.props.type} </Text>
-                </TouchableOpacity>
-        {/* <Button
-          title="Sign Up Yo!"
-          color=""
-          //onPress={() => Alert.alert('Simple Button pressed')}
-        /> */}
-      </View>           
-    
+        return (
+            <React.Fragment>
                 <View style={styles.container}>
-                <Text>{'\n'}</Text>
-                <Text>{'\n'}</Text>
-               {/* <Form type="Login"/> */}
-                <View style={styles.signupTextCont}> 
-                    <Text style={styles.signupText}>Dont have an account yet? </Text>
-                    <TouchableOpacity onPress={this.signup}><Text style={styles.signupButton}>Sign up!</Text></TouchableOpacity>
+                    <TextInput style={styles.inputBox} //creating email text input
+                        onChangeText={(email) => this.setState({ email })}
+                        underlineColorAndroid='rgba(0,0,0,0)'
+                        placeholder="Username"
+                        placeholderTextColor="#ff8700"
+                        selectionColor="#fff"
+                        keyboardType="Username"
+                        onSubmitEditing={() => this.password.focus()} />
+                    <TextInput style={styles.inputBox} //creating password text input
+                        onChangeText={(password) => this.setState({ password })}
+                        underlineColorAndroid='rgba(0,0,0,0)'
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        placeholderTextColor="#ff8700"
+                        ref={(input) => this.password = input}
+                    />
+                    <TouchableOpacity style={styles.button}>
+                        {
+                            //depending on what the user presses, the button will either render login or signup
+                        }
+                        <Text style={styles.buttonText} onPress={this.saveData}> Login {this.props.type} </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button1}>
+                        {
+                            //depending on what the user presses, the button will either render login or signup
+                        }
+                        <Text style={styles.buttonText1} onPress={this.saveData}> Sign Up!{this.props.type} </Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
             </React.Fragment>
         )
     }
 }
 
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       backgroundColor: 'white',
-//     },
-//     signupTextCont: {
-//       flexGrow: 1,
-//       justifyContent: 'center',
-//       alignItems: 'flex-end',
-//       paddingVertical: 16,
-//       flexDirection: 'row',
-//     },
-//     signupText: {
-//       color: '#12799f', 
-//       fontSize:16,
-//     },
-//     signupButton: {
-//         color: '#12799f',
-//         fontSize:16,
-//         fontWeight: '500',
-//     }
-// });
+
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center', //centers flex objects
