ccba2810d1d178fe679367f7a96a66c7bf917eaf
Harshitha <harshitha.arulm@gmail.com>
2019-11-02 14:25:37 -0700

    created requestedRides functionality in backend and stored updatedRides

--- backend/db.js
+++ backend/db.js
@@ -1,43 +1,102 @@
-// Define a custome databse
+// Custom database for the HitchHike backend
 
-const fs = require('fs');
+/* overview of structures
 
-// storing users in ram as array hashmap
-var __users = [] 
+* Users stored in hashmap with the key being their username
 
-// might want to store rides in k-d tree for efficient spacial searching
-var __rides = []
+* Users written to backup file upon signup
 
-// user object that will be stored in ran 
-function User(fName, lName, email, DOB){
-    this.userID = __users.length;
+* Rides store in two data structues:
+    * R-tree stores rideID according to 2D location
+    * rideID stored in hashmap where departure time is the key
+
+*/
+
+var security = require("./security.js");
+const fs = require('fs'); 
+var HashMap = require('hashmap');
+var Heap = require('heap');
+var RBush = require('rbush');
+var knn = require('rbush-knn');
+
+// storing users in hashmap where key is unique email
+var __users = new HashMap()
+
+// store ride objects in R tree for spacial lookup
+var __rides = new RBush();
+
+// custom comparator for the R tree data structure
+function comparator(a, b){
+    return a.Ride.RideID === b.Ride.RideID;
+}
+
+/*__rides.remove(item, (a, b) => {
+    return a.Ride.RideID === b.Ride.RideID;
+});*/
+
+// store departureTime: rideID
+var rideQueue = new HashMap()
+
+// user object that will be stored in ram
+function User(fName, lName, username, password, email, DOB){
+    this.username = username;
+    this.password = security.encryptPasword(password)
     this.fName = fName;
     this.lName = lName;
     this.email = email;
     this.DOB = DOB;
     this.rides = [];
+    this.requestedRides = [];
+    
+    //new users start unvarified
+    this.userStatus = {
+        verified: false    
+    }
 
-    // add to __users array, implicitly this makes the index = userID
-    __users.push(this)
+    __users.set(username, this)
 }
 
 // ride object
-function Rides(userID, origin, destination, seats, time){
-    this.rideID = __rides.length;
-    this.userID = userID;
+function Rides(username, origin, destination, seats, dateString){
+    this.rideID = module.exports.RideID(username, dateString);
     this.origin = origin;
     this.destination = destination;
     this.maxSeats = seats;
-    this.departTime = time;
-
-    __rides.push(this)
+    this.departTime = dateString
 }
 
+// create unique rideID
+// function RideID(username, date){
+//     departure = (date.getDay() + ":" + date.getHours() + ":" + date.getMinutes())
+//     return (username + ":" + departure)
+// }
+
+// remove rides who's departure time has passed
+function updateRides(){
+    var date = new Date()
+
+    //console.log("updating rides")
+    key = (date.getDay() + ":" + date.getHours() + ":" + date.getMinutes())
+
+    if (rideQueue.has(key)){
+
+        rideID = rideQueue.get(key)
+        rideQueue.deleted(key)
+        __rides.remove(rideID)
+
+        console.log("ride with ID=", rideID, "has expired, moving it to pastRides")
+    }
+}
+
+// make updateRides run every 30 seconds 
+let timerId = setInterval(() => updateRides(), 30000);
+
 // find user in __users
-function findUser(userID){
-    try{ return __users[userID] }
+function findUser(username){
+    try{ return __users.get(username)}
     catch(e){
         console.log(e)
+        return e
     }
 }
 
@@ -55,13 +114,13 @@ function write_to_file(user_obj){
             if (err) throw err;
         });
 
-        console.log(backup)
+        //console.log(backup)
     })
 }
 
 // read users from file
-function readBackup(userID){
-    console.log("Warning: we are either filling __users or overriding __users from disk")
+function readBackup(username, status){
+    console.log("usage: Reading __users from disk")
     
     var text = fs.readFileSync('backup.json')
     var file = JSON.parse(text)
@@ -69,15 +128,15 @@ function readBackup(userID){
     for (var i=0; i<file['users'].length; i++){
         
         // seach for single user
-        if (userID >= 0){ 
-            if (file['users'][i]['userID'] == userID){
-                console.log("found userID")
+        if (status >= 0){ 
+            if (file['users'][i]['username'] == username){
+                console.log("found user")
                 return file['users'][i]
             }
         }
         // transfer entire backup file to __users array
         else{
-            __users.push(file['users'][i])
+            __users.set(username, file['users'][i])
             return
         }
     }
@@ -86,72 +145,184 @@ function readBackup(userID){
 // public functions availiable to index.js
 module.exports = {
 
-    newUser: function(fName, lName, email, DOB){
+    newUser: function(fName, lName, username, password, email, DOB){
         console.log("creating new user")
-        var user = new User(fName, lName, email, DOB)
 
-        // write user to backup file
-        //console.log(user)
+        if (__users.has(username)){
+            throw Error ('username in use')
+        }
+
+        var user = new User(fName, lName, username, password, email, DOB)
+
+        // writing user to backup immediately for now
+        console.log(user)
         write_to_file(user)
+
+        return user
     },
 
-    getUser: function(userID){
-
-        if (userID >= __users.length || userID < 0){
-            // if ram db crashed
-            if (__users.length == 0){
-                readBackup(-1)
-                user = findUser(userID)
-                console.log(user)
-                return user
-            }
-            console.log("userID is not in users database")
-        }
-        else{
-            user = findUser(userID)
+    getUser: function(username){
+        
+        // if db crashed, read from file
+        if (__users.size == 0){
+            readBackup(username, -1)
+            user = findUser(username)
             console.log(user)
             return user
         }
-
+        else{
+            user = findUser(username)
+            //console.log(user)
+            return user
+        }
     },
 
-    updateUser: function(userID, field, oldP, newP){
-        console.log("update user")
+    updateUser: function(username, field, oldP, newP){
+        console.log("updating user")
         try{
-            user = module.exports.getUser(userID)
-            user['field'] = newP
-            __users[userID] = user
+            user = module.exports.getUser(username)
+            if (field == "password"){
+                security.updatePassword(username, oldP, newP)
+            }
+            user[field] = newP
+            __users.set(username, user)
         }
         catch(e){
-            console.log("was not able to update userID", userID)
+            console.log("was not able to update user", username, e)
         }
     },
 
-    deleteUser: function(userID){
-        console.log("remove user")
+    deleteUser: function(username){
+        console.log("removing user from database")
+
+        if (__users.has(username)){
+            __users.delete(username)
+            console.log("user deleted successfuly")
+        }
+        else{
+            console.log("could not delete user because email was not found")
+        }
     },
 
-    postRide: function(userID, origin, destination, time){
-        console.log("post a ride from x to y at time t")
+    // date is in format: "August 19, 1975 23:15:30"
+    postRide: function(username, origin, destination, seats, dateString){
+        
+        console.log("posting a ride from x to y at time t")
+        user = module.exports.getUser(username)
+
+        /*if (user.userStatus.verified === false){
+            throw Error ("must be verified to post a ride")
+        }*/
+
+        date = new Date(dateString)
+
+        // store by day, hour, minutes
+        departure = (date.getDay() + ":" + date.getHours() + ":" + date.getMinutes())
+
+        // create the ride
+        var ride = new Rides(username, origin, destination, seats, date)
+
+        // add rideID to hashmap of rides
+        rideQueue.set(departure, ride.rideID)
+
+        // add rideID to user's rides attribute
+        user.rides.push(ride.rideID)
+
+        const node = {
+            minX: origin.x,
+            minY: origin.y,
+            maxX: origin.x,
+            maxY: origin.y,
+            Ride: ride
+        }
+
+        __rides.insert(node);
+        // console.log(ride)
     },
 
-    deleteRide: function(userID, rideID){
-        console.log("cancel a ride given its ID")
+    requestRide: function(username, ride){
+        var rideList = __rides.search({
+            minX: ride.origin.x,
+            minY: ride.origin.y,
+            maxX: ride.origin.x,
+            maxY: ride.origin.y,
+        })
+
+        if (rideList.length > 0){
+            for (var i = 0; i < rideList.length; i++){
+                if (rideList[i].data.children[0].Ride.rideID == ride.rideID){
+                    this.getUser(username).requestedRides.push
+                }
+            }
+        }
+        return 
     },
 
-    updateRide: function(userID, rideID){
-        console.log("update a posted ride")
+    deleteRide: function(username){
+        try{
+            user = module.exports.getUser(username)
+            rideID = user.rides[user.rides.length-1]
+            __rides.remove(rideID)
+            console.log("successfuly deleted ride for", username)
+        }
+        catch{
+            throw Error ("could not remove", rideID, "from database")
+        }
     },
 
-    findRide: function(userID, location, time){
-        console.log("find all rides near me")
+    updateRide: function(username, rideID, origin, destination, seats, departure){
+        try{
+          node = __rides.remove(rideID).data.children[0].Ride
+        //   console.log(node)
+          node.origin = origin
+          node.destination = destination
+          node.seats = seats
+          node.departure = departure
+          __rides.insert(node) 
+        }
+        catch{
+            throw Error ("could not update ride from database")
+        }
     },
 
-    testRead: function(userID){
-        read_from_file(userID)
+    findRide: function(location, dateString){
+        
+        console.log("looking for rides")
+
+        var date = new Date(dateString)
+        var buffer = 2 // two hour windows
+
+        var rides = knn(__rides, location.x, location.y, function (item) {
+            
+            // return item if within date/hour range
+            if (item.departTime.getDay() == date.getDay()){
+                if ((item.departTime.getHours() - (date.getHours()+buffer)) > 0){
+                    return item
+                }
+                else if ((item.departTime.getHours() - (date.getHours()-buffer)) > 0){
+                    return item
+                }
+            }
+
+        });
+
+        console.log(rides)
     },
 
+    testBackup: function(username){
+        console.log("size of database before backup read", __users.size)
+        readBackup(username, -1)
+        console.log("size after backup read", __users.size)
+    },
 
+    hash: function(password){
+        return security.encryptPasword(password)
+    },
+
+    RideID: function(username, date){
+        departure = (date.getDay() + ":" + date.getHours() + ":" + date.getMinutes())
+        return (username + ":" + departure)
+    }
 }
 
 
