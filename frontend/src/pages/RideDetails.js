164eabc3fb16791cd71f36d518334f8df5e4fdb5
Harshitha <harshitha.arulm@gmail.com>
2019-11-19 21:03:55 -0800

    Added Ride Details Page

---
+++ frontend/src/pages/RideDetails.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import { Card } from 'react-native-paper';
+
+export default class MyRides extends Component {
+    getRating = function(username) {
+        var rating = null;
+        axios.post('http://ec2-13-59-36-193.us-east-2.compute.amazonaws.com:8000/getRating',{username: username})
+        .then(function (response){
+            rating = response.data
+        })
+        return rating;
+    }
+  render() {
+    
+    return (
+        //Surya: please get the ride from the ride selction page while routing. This just has dummy info
+      <React.Fragment>
+        <View>
+          <Text style={styles.title}>My Rides</Text>
+        </View>
+        <View style={styles.container}>
+          {rides.map(ride => {
+            return (
+              <Card style={styles.rideCard}>
+                <View style={styles.cardRow}>
+                  <Text>{ride.origin.desc}</Text>
+                  <Image style={{width: 40, height: 100,marginLeft:30,marginRight:30,marginBottom:10}} source={require('../../assets/arrow_right.png')}/>
+                  <Text>{ride.destination.desc}</Text>
+                </View>
+                <View style={styles.cardRow}>
+                  <Text>Departs at {ride.departTime}</Text>
+                </View>
+                <View style={styles.cardRow}>
+                  <Text>Rider's Phone Number{ride.riderPhone}</Text>
+                </View>
+                <View style={styles.cardRow}>
+                  <Text>Driver's Rating{this.getRating(ride.driverUserName)}</Text>
+                </View>
+                <View style={styles.cardRow}>
+                  <Text>Number of seats left:{ride.seatsLeft}</Text>
+                </View>
+              </Card>
+            );
+          })}
+        </View>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    padding: 20,
+    marginTop:20,
+    color: '#ff8700',
+  },
+  rideCard: {
+    padding: 20,
+    margin: 10,
+    marginTop:0,
+    alignSelf: 'stretch',
+    shadowRadius:5
+  },
+  cardRow: {
+    flexDirection:"row",
+    justifyContent:"center",
+  }
+});
+
+
