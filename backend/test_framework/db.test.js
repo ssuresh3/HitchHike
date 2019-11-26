12588776166ba35dad4c80ae6922e403f8150259
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-25 19:13:12 -0800

    fixed variable name error in db and added test cases

--- backend/test_framework/db.test.js
+++ backend/test_framework/db.test.js
@@ -2,29 +2,35 @@ const fs = require('fs');
 const db = require('../db');
 
 var mockData = require('./mock_data.json')
-var user = mockData['user']
 
 // test new user
 test('the data is a new user', () => {
-  expect(db.newUser(user.fName, user.lName, user.username, user.password, user.email, user.pNumber, user.DOB).email).toBe('chartsoo@ucsc.edu')
+	var user = mockData['users'][0]
+ 	expect(db.newUser(user.fName, user.lName, user.username, user.password, user.email, user.pNumber, user.DOB).email).toBe('chartsoo@ucsc.edu')
 });
 
 // test get user
 test('the data is a new user', () => {
-  expect(db.getUser(user.username).email).toBe('chartsoo@ucsc.edu')
+	var user = mockData['users'][0]
+ 	expect(db.getUser(user.username).email).toBe('chartsoo@ucsc.edu')
 });
 
 // test posting a ride and finding a ride
 test('posting a ride', () => {
-
-	var origin = {"x":90, "y":-100}
-	var destination = {"x": 70, "y": -104}
-	var seats = 4
-	var time = "August 19, 2020 23:15:30"
-	db.postRide(user.username, origin, destination, seats, time)
-
-	db.findRide(origin, time)
-	expect(db.getUser(user.username).email).toBe('chartsoo@ucsc.edu')
+	var user = mockData['users'][0]
+	var ride = mockData['rides'][0]
 	
-})
+	db.postRide(user.username, ride.origin, ride.destination, ride.seats, ride.departure)
+	expect(db.getUser(user.username).email).toBe('chartsoo@ucsc.edu')
+});
+
+// test find ride
+test('finding posted ride', () => {
+	var user = mockData['users'][0]
+	var ride = mockData['rides'][0]
+	var request = mockData['requestedRides'][0]
+
+	var rideFound = db.findRide(request.origin, request.departure)
+	expect(rideFound).toBeDefined()
+});
 
