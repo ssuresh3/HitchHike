ba71adc71218b35c1f85ec7d2ec08a35f8d9e70f
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2019-12-01 15:31:21 -0800

    update allrides function

--- backend/db.js
+++ backend/db.js
@@ -354,7 +354,6 @@ module.exports = {
         var ridesArray = [];
         if (__rides.data.children != null) {
             __rides.data.children.forEach(child => {
-                console.log(child);
                 if (child.Ride != null && child.Ride.seatsLeft > 0) ridesArray.push(child.Ride);
             });
         }
