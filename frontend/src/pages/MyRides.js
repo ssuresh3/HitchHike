30c53293385993cd09aaebf5b711eb83d219591e
harshithaarul <harshitha.arulm@gmail.com>
2019-12-02 02:27:17 -0800

    changed myRides.js

--- frontend/src/pages/MyRides.js
+++ frontend/src/pages/MyRides.js
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 import Divider from 'react-native-divider'
-
+import {myRides} from "./Styles"
 class RideView extends React.Component {
   render() {
     return (
@@ -23,7 +18,6 @@ class RideView extends React.Component {
     );
   }
 }
-
 export default class App extends React.Component {
   render() {
     return (
@@ -44,7 +38,6 @@ export default class App extends React.Component {
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
