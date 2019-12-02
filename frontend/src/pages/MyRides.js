a42f190f88d318bd40b40515794cf6f68c2ce347
prasadaman2000 <prasa@amaprasa.localdomain>
2019-12-02 02:13:29 -0800

    really bad template for myrides

--- frontend/src/pages/MyRides.js
+++ frontend/src/pages/MyRides.js
@@ -1,111 +1,62 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, AsyncStorage } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
-import  Divider  from 'react-native-divider'
-import {myRides} from './Styles'
+import Divider from 'react-native-divider'
 
-export default class MyRides extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      rides: [],
-    };
-    this.displayName() 
-  }
-
-  displayName = async () => {
-    try {
-      let user = await AsyncStorage.getItem('user');
-      let parsedUser = JSON.parse(user);
-      this.setState({rides:parsedUser.data.rides});
-      console.log("extracting from asyncstorage")
-      console.log(this.state.rides);
-      // <Text>user</Text>
-    } catch (error) {
-      alert(error)
-    }
-  }
-
+class RideView extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Divider borderColor="#ff8700" color="#ff8700" orientation="center">
-          <Text>Posted Rides</Text>
-        </Divider>
-        {/*<View style={myRides.container}>
-          {this.state.rides.map(ride => {
-            return (
-              // <Card style={myRides.rideCard}>
-              //   <View style={myRides.cardRow}>
-              //     <Text>{ride.origin.desc}</Text>
-              //     <Image style={{width: 30, height: 20,marginLeft:30,marginRight:30,marginBottom:10}} source={require('../../assets/arrow_right.png')}/>
-              //     <Text>{ride.destination.desc}</Text>
-              //   </View>
-              //   <View style={myRides.cardRow}>
-              //     <Text>Departs at {ride.departTime}</Text>
-              //   </View>
-              //   <View style={myRides.cardRow}>
-              //     <Text>Rider's Phone Number{ride.riderPhone}</Text>
-              //   </View>
-              // </Card>
-              <Text>Hi</Text>
-            );
-          })}
-        </View>
-         <View>
-            <Divider borderColor="#ff8700" color="#ff8700" orientation="center"/>
-              <Text>Hi</Text>
-            </Divider>
-        </View> */}
-      </React.Fragment>
+      <View>
+        <Card>
+          <Text>{this.props.driverName}</Text>
+          <Text>{this.props.origin}</Text>
+          <Text>{this.props.destination}</Text>
+          <Text>{this.props.time}</Text>
+        </Card>
+      </View>
     );
   }
 }
 
-// const myRides = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   title: {
-//     fontSize: 30,
-//     textAlign: 'center',
-//     padding: 20,
-//     marginTop: 20,
-//     color: '#ff8700',
-//   },
-//   rideCard: {
-//     padding: 20,
-//     margin: 10,
-//     marginTop: 0,
-//     alignSelf: 'stretch',
-//     shadowRadius: 5
-//   },
-//   cardRow: {
-//     flexDirection: "row",
-//     justifyContent: "center",
-//   }
-// });
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Divider borderColor="#fff870" color="black" orientation="center">
+            Your Posted Rides
+        </Divider>
+        <RideView
+          driverName={'Bitchass'}
+          origin={'Bitch'}
+          destination={'Ass'}
+          time={'When the clock strikes 9'}
+        />
+        <Divider borderColor="#fff870" color="black" orientation="center">
+            Your Booked Rides
+        </Divider>
+      </View>
+    );
+  }
+}
 
-// const rides = [
-//   {
-//     origin: 'Crown',
-//     destination: 'RCC',
-//     maxSeats: 5,
-//     departTime: '12:30',
-//   },
-//   {
-//     origin: 'McHenry',
-//     destination: 'SNE',
-//     maxSeats: 5,
-//     departTime: '12:50',
-//   },
-//   {
-//     origin: 'UCSC',
-//     destination: 'Downtown',
-//     maxSeats: 5,
-//     departTime: '6:40',
-//   },
-// ];
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
