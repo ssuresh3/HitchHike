ddc38bad41bb3f48b02954bdf52327be0c45db92
prasadaman2000 <prasa@amaprasa.localdomain>
2019-12-02 03:04:33 -0800

    removed random stuff from myrides

--- frontend/src/pages/MyRides.js
+++ frontend/src/pages/MyRides.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-// You can import from local files
-import AssetExample from './components/AssetExample';
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 import Divider from 'react-native-divider'
